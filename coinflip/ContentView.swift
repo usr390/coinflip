ba5d73ad45fe00f4ec29e8585db2e52d00c52849
vendingcompute <edutre1999@gmail.com>
2025-01-30 13:17:02 -0600

    Initial Commit

---
+++ coinflip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coinflip
+//
+//  Created by abs on 1/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coinflip/coinflipApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinflipApp.swift
+//  coinflip
+//
+//  Created by abs on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct coinflipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
