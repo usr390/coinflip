0d104ba542986f1292a46c34da99221ba3b0a704
vendingcompute <edutre1999@gmail.com>
2025-02-25 11:40:53 -0600

    initial commit

--- coinflip/ContentView.swift
+++ coinflip/ContentView.swift
@@ -1,24 +1,44 @@
-//
-//  ContentView.swift
-//  coinflip
-//
-//  Created by abs on 1/30/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var coinSide: String = "heads"
+    @State private var spinDegrees: Double = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // full-screen tappable area
+            Color.black.opacity(0.001)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    flipCoin()
+                }
+
+            // coin image with its own tap gesture
+            Image(coinSide)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .rotation3DEffect(
+                    Angle(degrees: spinDegrees),
+                    axis: (x: 0, y: 1, z: 0)
+                )
+                .animation(.easeInOut(duration: 0.6), value: spinDegrees)
+                .onTapGesture {
+                    flipCoin()
+                }
         }
-        .padding()
+    }
+
+    func flipCoin() {
+        spinDegrees += 360
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            coinSide = Bool.random() ? "heads" : "tails"
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
